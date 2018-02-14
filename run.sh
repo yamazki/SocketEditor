dfa78d5c304bc48ad097d3a3354b073956619b64
yamazaki <yamazaki@ik1-330-25500.vs.sakura.ne.jp>
2018-02-15 01:47:32 +0900

    docker compose use

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker build -t nodeapp/yamazaki:1.0 .
-
-docker run -p 1337:1337 -v /home/yamazaki/socketEditor:/root nodeapp/yamazaki ./node_modules/.bin/nodemon
-
--- test.sh
+++
@@ -1,6 +0,0 @@
-ID=$(docker images | awk '$1 == "nodeapp/yamazaki" {print $3}')
-
-echo $ID  
-
-docker run -p 1337:1337 -v /home/yamazaki/socketEditor:/root $ID ./node_modules/.bin/nodemon
-
